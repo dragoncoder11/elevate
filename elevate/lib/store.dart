b9a018b40448b67220ca05f72ce98e9a788ccc2a
John Doe <johndoe@email.com>
2024-09-24 03:19:56 +0300

    add launcher icon to the app and flutter native splash

---
+++ elevate/lib/store.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+class StoreApp extends StatelessWidget {
+  const StoreApp({super.key,});
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(390, 844),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return const MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Locked App',
+        
+        );
+      },
+    );
+  }
+}
